e2ae12fd25467b6f5d4db92974240c66eee20c3f
Ryan James Gregoire <ryangregoire286@gmail.com>
2024-12-29 14:44:25 -0500

    Learned About Objects

---
+++ Scope and Hoisting/Hosting.js
@@ -0,0 +1,14 @@
+function main() {
+  name();
+  //   myName();
+
+  function name() {
+    console.log("RJ");
+  }
+
+  //   var myName = function() {
+  //     console.log("Hello I am: Ryan");
+  //   };
+}
+
+main();
---
+++ Scope and Hoisting/scope.js
@@ -0,0 +1,33 @@
+// var x = 13;
+
+// function test() {
+//   var x = 23;
+
+//   if (x > 20) {
+//     var x = 50 + 5;
+//   }
+
+//   console.log(x);
+// }
+
+// test();
+
+// var x = 90;
+// console.log(x);
+
+// if (true) {
+//   var x = 20;
+//   console.log(x);
+// }
+
+var x = 420.0;
+console.log(x);
+
+if (true) {
+  (function() {
+    var x = 12;
+    console.log(x);
+  })();
+}
+
+console.log(x);
