ebd066e3130515ec419b57507144d32e261b4aba
Ryan James Gregoire <ryangregoire286@gmail.com>
2024-12-29 09:40:49 -0500

    Learned About Objects

---
+++ Objects/main.js
@@ -0,0 +1,13 @@
+let myPerson = {
+  name: "John Doe",
+  age: 29,
+  isOlder: false
+};
+
+// console.log(myPerson.age);
+// console.log(myPerson.isOlder);
+// console.log(myPerson.name);
+
+console.log(myPerson["age"]);
+console.log(myPerson["name"]);
+console.log(myPerson["isOlder"]);
