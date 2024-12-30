83ff1c53d9e628af080ac63b907f0610f7442a2e
Ryan James Gregoire <ryangregoire286@gmail.com>
2024-12-30 10:17:14 -0500

    Added And Learned About Error Handling

---
+++ Errir Handling/ErrorHandling.js
@@ -0,0 +1,10 @@
+try {
+  var name1 = "Ryan";
+  var lastName = "James";
+  var age = 2024 - 1999;
+  console.log("Name:", name1);
+  console.log("Last Name:", lastName);
+  console.log("Age:", age);
+} catch (e) {
+  console.log("Error", e);
+}
